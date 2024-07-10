be7de6ddfec9bb9fc49c952b126de964ff053bb7
Chris Roberts <chrisrobertscollective@gmail.com>
2024-07-10 10:45:08 +0200

    adding tailwind config

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aura</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Aura</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{
         color: 'white',
@@ -19,12 +19,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
