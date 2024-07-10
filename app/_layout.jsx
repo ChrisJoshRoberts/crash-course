b99e259f4cf32e5bdd2a9c3253fc365d47a88960
Chris Roberts <chrisrobertscollective@gmail.com>
2024-07-10 12:16:14 +0200

    adding all jsx pages

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native'
-import { Slot, Stack } from 'expo-router'
+import { Slot, SplashScreen, Stack } from 'expo-router'
 import { useFonts } from 'expo-font'
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
@@ -14,6 +17,14 @@ const RootLayout = () => {
     "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
     "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
   });
+
+  useEffect(() => {
+    if (error)  throw error
+    if (fontsLoaded) SplashScreen.hideAsync();
+  },[fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) return null;
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{headerShown: false}}/>
