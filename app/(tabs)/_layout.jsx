73d000b95701866204b2f985c6d649c80f0b7539
Chris Roberts <chrisrobertscollective@gmail.com>
2024-07-11 22:52:47 +0200

    adding home icon

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,5 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import { Tabs, Redirect } from 'expo-router'
+
+const TabIcon = ({icon, color, name, focused}) => {
+  return (
+    <View>
+      <Image
+        source={icon}
+      />
+    </View>
+  )
+
+}
 
 
 const Layout = () => {
@@ -8,6 +19,15 @@ const Layout = () => {
       <Tabs>
         <Tabs.Screen
         name="home"
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: (color, focuses) => (
+            <TabIcon
+              icon= {}
+            />
+          )
+        }}
         />
       </Tabs>
     </>
