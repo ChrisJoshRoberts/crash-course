84b7a06cee85d9d8299900054fa353e1a6761bff
Chris Roberts <chrisrobertscollective@gmail.com>
2024-07-22 12:30:07 +0100

    adding sign in

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -7,7 +7,7 @@ const FormField = ({title, value, placeholder, handleChangeText, otherStyles, ..
   return (
     <View className={`${otherStyles} space-y-2`}>
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
-      <View className=" border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl items-center focus:border-secondary flex-row">
+      <View className="  border-2 w-full h-16 px-4 bg-black-50 rounded-2xl items-center focus:border-secondary flex-row">
         <TextInput
         className="flex-1 text-white font-psemibold text-base"
         value={value}
